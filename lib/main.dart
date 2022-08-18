b1b36a5bc4a7b9c63376988ec42c64c7d0626c4d
Trickster811 <nedaoukajoachim@gmail.com>
2022-08-18 09:27:32 +0100

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,7 @@ import 'package:flutter/services.dart';
 import 'package:icloud/welcome_screens/loading_page.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]).then(
-    (value) => runApp(
-      const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -27,8 +19,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
-      title: 'iCloud',
+      title: 'One Cloud',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Color.fromARGB(255, 243, 243, 243),
